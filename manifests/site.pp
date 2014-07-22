1438d6386724f01a3fb741b3380c524ecf14c8e5
Dan <bodepd@gmail.com>
2014-07-21 19:44:00 -0700

    Move jenkins and zuul config to ci profiles

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,107 +1,12 @@
 
-$jenkins_username = 'jenkins_user'
-$jenkins_password = 'jenkins_password'
 
 $jenkins_server = 'http://11.2.3.11:8080'
 
 if $::role == 'jenkinsserver' {
 
-  include puppet_openstack_tester::zuul
+  include ci_profiles::jenkins
 
-  include puppet_openstack_tester::puppet_jobs
-
-  Service <| title == 'zuul' |> {
-    ensure  => running,
-    require +> Class['zuul'],
-  }
-
-  Service <| title == 'zuul-merger' |> {
-    ensure  => running,
-    require => Service['zuul'],
-  }
-
-  file { '/etc/jenkins_jobs/config':
-    ensure => directory,
-    before => Exec['jenkins_jobs_update'],
-  }
-
-  jenkins::plugin { 'ansicolor':
-    version => '0.3.1',
-  }
-
-  include jenkins, jenkins::master
-
-  Service['jenkins'] ~> Exec['sleep_two_min'] -> Exec['jenkins_jobs_update']
-
-  exec { 'sleep_two_min':
-    command     => '/bin/sleep 120',
-    refreshonly => true,
-  }
-
-  class { 'jenkins_job_builder':
-    url      => 'http://127.0.0.1:8080',
-    username => $jenkins_user,
-    password => $jenkins_password,
-    require  => Package['git']
-  }
-
-  jenkins::plugin { 'gearman-plugin':
-    version => '0.0.3',
-  }
-  jenkins::plugin { 'git':
-    version => '1.1.23',
-  }
-  jenkins::plugin { 'htmlpublisher':
-    version => '1.0',
-  }
-  jenkins::plugin { 'jobConfigHistory':
-    version => '1.13',
-  }
-  jenkins::plugin { 'notification':
-    version => '1.4',
-  }
-  jenkins::plugin { 'openid':
-    version => '1.5',
-  }
-  jenkins::plugin { 'publish-over-ftp':
-    version => '1.7',
-  }
-  jenkins::plugin { 'rebuild':
-    version => '1.14',
-  }
-  jenkins::plugin { 'simple-theme-plugin':
-    version => '0.2',
-  }
-  jenkins::plugin { 'timestamper':
-    version => '1.3.1',
-  }
-  jenkins::plugin { 'token-macro':
-    version => '1.5.1',
-  }
-  jenkins::plugin { 'urltrigger':
-    version => '0.24',
-  }
-
-  package { 'git':
-    ensure => installed,
-  }
-
-  class { '::zuul':
-    vhost_name           => $::ipaddress_eth2,
-    gearman_server       => '127.0.0.1',
-    gerrit_server        => 'review.openstack.org',
-    # I need to create another user for this so that I don't have to use my own...
-    gerrit_user          => hiera('gerrit_user', 'puppet-openstack-ci-user'),
-    # private key for gerrit user
-    zuul_ssh_private_key => hiera('zuul_ssh_private_key'),
-    url_pattern          => '',
-    zuul_url             => '',
-    job_name_in_report   => true,
-    status_url           => "http://${::ipaddress_eth2}/",
-    #statsd_host          => $statsd_host,
-    #git_email            => 'jenkins@openstack.org',
-    #git_name             => 'OpenStack Jenkins',
-  }
+  include ci_profiles::zuul
 
 } elsif $::role == 'jenkinsclient' {
   # install a jenkins slave configured to launch jobs using heat
